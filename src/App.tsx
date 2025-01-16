8ddac8f3d86c4d031bd0ea92ba36de3c5574b53a
hlainghpone1 <hlainghpone1@gmail.com>
2025-01-16 09:27:03 -0800

    Project Setup

---
+++ src/App.tsx
@@ -0,0 +1,14 @@
+function App() {
+	return (
+		<>
+			<div className="ml-4 flex h-24 border-2 border-gray-300 p-4 pt-2 text-gray-700 shadow-md">
+				Lorem, ipsum dolor sit amet consectetur adipisicing elit. Re
+				cusan dae atque, et temporibus labore magnam reiciendis nulla
+				saepe inventore sit omnis, a impedit suscipit fuga? Laboriosam
+				hic blanditiis qui busd am minus vero?
+			</div>
+		</>
+	);
+}
+
+export default App;
