54284e3bd8830fec0ff592bacb2285eea91bf4f4
hlainghpone1 <hlainghpone1@gmail.com>
2025-01-18 19:35:27 -0800

    Set Up Project Folders and Add dependencies

---
+++ src/api/index.ts
@@ -0,0 +1,35 @@
+import axios from 'axios';
+
+export const baseURL = import.meta.env.VITE_BASE_URL + '/api';
+
+const api = axios.create({
+	baseURL: baseURL,
+	headers: {
+		ContentType: 'application/json',
+		'Cache-Control': 'no-cache',
+		'Access-Control-Allow-Origin': '*',
+		Accept: 'application/json',
+	},
+});
+
+api.interceptors.request.use(
+	(request) => {
+		request.headers['X-Request-Start-Time'] = Math.floor(Date.now() / 1000);
+
+		return request;
+	},
+	(error) => {
+		return Promise.reject(error);
+	}
+);
+
+api.interceptors.response.use(
+	(response) => {
+		return response;
+	},
+	(error) => {
+		return Promise.reject(error);
+	}
+);
+
+export default api;
