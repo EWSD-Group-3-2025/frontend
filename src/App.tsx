5497a86f3510ed7ce8e003ea7b344bf73d87f04e
hlainghpone1 <hlainghpone1@gmail.com>
2025-01-25 00:04:12 -0800

    Feat (components) Integrate Toaster component and update PasswordInput functionality

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,11 @@
+import { Toaster } from '@/components/sonner';
 import Router from '@/router';
 
 function App() {
 	return (
 		<>
 			<Router />
+			<Toaster position="top-center" richColors closeButton />
 		</>
 	);
 }
--- src/components/password-input.tsx
+++ src/components/password-input.tsx
@@ -1,39 +1,55 @@
-import { forwardRef, useState } from 'react';
-import { Eye, EyeOff } from 'lucide-react';
+import * as React from 'react';
+import { EyeIcon, EyeOffIcon } from 'lucide-react';
 import { Input } from '@/components/input';
+import { cn } from '@/utils/stringUtils';
 import { Button } from '@/components/button';
 
-interface PasswordInputProps
-	extends React.InputHTMLAttributes<HTMLInputElement> {
-	placeholder?: string;
-}
+const PasswordInput = React.forwardRef<
+	HTMLInputElement,
+	React.InputHTMLAttributes<HTMLInputElement>
+>(({ className, ...props }, ref) => {
+	const [showPassword, setShowPassword] = React.useState(false);
+	const disabled =
+		props.value === '' || props.value === undefined || props.disabled;
 
-const PasswordInput = forwardRef<HTMLInputElement, PasswordInputProps>(
-	({ placeholder, ...props }, ref) => {
-		const [showPassword, setShowPassword] = useState(false);
+	return (
+		<div className="relative">
+			<Input
+				type={showPassword ? 'text' : 'password'}
+				className={cn('hide-password-toggle pr-10', className)}
+				ref={ref}
+				{...props}
+			/>
+			<Button
+				type="button"
+				variant="ghost"
+				size="sm"
+				className="absolute right-0 top-0 h-full px-3 py-2 hover:bg-transparent"
+				onClick={() => setShowPassword((prev) => !prev)}
+				disabled={disabled}
+			>
+				{showPassword && !disabled ? (
+					<EyeIcon className="h-4 w-4" aria-hidden="true" />
+				) : (
+					<EyeOffIcon className="h-4 w-4" aria-hidden="true" />
+				)}
+				<span className="sr-only">
+					{showPassword ? 'Hide password' : 'Show password'}
+				</span>
+			</Button>
 
-		return (
-			<div className="relative">
-				<Input
-					type={showPassword ? 'text' : 'password'}
-					placeholder={placeholder || 'Enter your password'}
-					{...props}
-					ref={ref}
-				/>
-				<Button
-					type="button"
-					onClick={() => setShowPassword(!showPassword)}
-					variant="ghost"
-					size="icon"
-					className="absolute right-2 top-1/2 -translate-y-1/2 transform hover:bg-transparent"
-				>
-					{showPassword ? <EyeOff size={16} /> : <Eye size={16} />}
-				</Button>
-			</div>
-		);
-	}
-);
-
+			{/* hides browsers password toggles */}
+			<style>{`
+					.hide-password-toggle::-ms-reveal,
+					.hide-password-toggle::-ms-clear {
+						visibility: hidden;
+						pointer-events: none;
+						display: none;
+					}
+				`}</style>
+		</div>
+	);
+});
 PasswordInput.displayName = 'PasswordInput';
 
-export default PasswordInput;
+export { PasswordInput };
