2cafa4ef87a218019e03d8d3e692515dc326bf0a
hlainghpone1 <hlainghpone1@gmail.com>
2025-01-25 00:08:08 -0800

    Feat (components) Create index file to export common components

---
+++ src/components/index.ts
@@ -0,0 +1,33 @@
+import { Button, ButtonProps } from '@/components/button';
+import {
+	FormItem,
+	FormLabel,
+	FormControl,
+	FormMessage,
+	FormDescription,
+	Form,
+	FormField,
+	useFormField,
+} from '@/components/form';
+import { Input } from '@/components/input';
+import { Label } from '@radix-ui/react-label';
+import { PasswordInput } from '@/components/password-input';
+import { Toaster } from '@/components/sonner';
+
+export {
+	Input,
+	Label,
+	Button,
+	FormItem,
+	FormLabel,
+	FormControl,
+	FormMessage,
+	PasswordInput,
+	FormDescription,
+	Form,
+	Toaster,
+	FormField,
+	useFormField,
+};
+
+export type { ButtonProps };
