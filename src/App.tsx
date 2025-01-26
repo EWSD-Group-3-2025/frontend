ea8b727273e8dcd651143593068d3a26f877f8d5
hlainghpone1 <hlainghpone1@gmail.com>
2025-01-26 00:20:25 -0800

    add

--- src/App.tsx
+++ src/App.tsx
@@ -4,6 +4,10 @@ import Router from '@/router';
 function App() {
 	return (
 		<>
+			Lorem, ipsum dolor sit amet consectetur adipisicing elit. Voluptatem
+			aut eius sit quo temporibus voluptates id ea recusandae fugiat
+			libero dolore rerum animi natus, laudantium consequuntur harum
+			consequatur. Neque, obcaecati.
 			<Router />
 			<Toaster position="top-center" richColors closeButton />
 		</>
