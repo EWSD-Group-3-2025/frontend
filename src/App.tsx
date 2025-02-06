b2db8d25a1eede0e03a840b579f04684d665bddf
Kei-K23 <keiksl2301@gmail.com>
2025-02-06 10:24:39 +0630

    feat: setup dark/light mode theme

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,17 @@
 import Router from '@/router';
 import { Toaster } from './components/ui/sonner';
+import { ThemeProvider } from './components/theme-provider';
 
 function App() {
 	return (
 		<>
-			<Router />
-			<Toaster position="top-center" closeButton />
+			<ThemeProvider
+				defaultTheme="system"
+				storageKey="ewsd-frontend-theme"
+			>
+				<Router />
+				<Toaster position="top-center" closeButton />
+			</ThemeProvider>
 		</>
 	);
 }
