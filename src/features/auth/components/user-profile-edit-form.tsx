43551d807fe7cbc2943de5bd76f20a40d2b1326e
Kei-K23 <keiksl2301@gmail.com>
2025-02-06 15:40:59 +0630

    chore: remove unuse variable

--- src/features/auth/components/user-profile-edit-form.tsx
+++ src/features/auth/components/user-profile-edit-form.tsx
@@ -8,7 +8,6 @@ import {
 	FormMessage,
 } from '@/components/ui/form';
 import { Input } from '@/components/ui/input';
-import { useAuth } from '@/context/auth.context';
 import { zodResolver } from '@hookform/resolvers/zod';
 import { useEffect, useState } from 'react';
 import { useForm } from 'react-hook-form';
