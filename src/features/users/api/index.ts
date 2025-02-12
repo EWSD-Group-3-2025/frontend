1bd9b7c7473368b8a7380a429e9fadab336f13bd
hlainghpone1 <hlainghpone1@gmail.com>
2025-02-12 09:44:27 -0800

    feat: add user management API functions for creating, updating, and retrieving users

--- src/features/users/api/index.ts
+++ src/features/users/api/index.ts
@@ -3,6 +3,8 @@ import CONSTANTS from '@/constants';
 import api from '@/utils/axios';
 import { ChangePasswordRequest } from '../types';
 import userRoutes from './routes';
+import { UserFormValue } from '@/features/users/pages/admin/UserForm';
+import { buildURL } from '@/utils';
 
 export const userChangePassword = async (body: ChangePasswordRequest) => {
 	const refreshToken = Cookies.get(CONSTANTS.REFRESH_TOKEN_KEY);
@@ -10,3 +12,15 @@ export const userChangePassword = async (body: ChangePasswordRequest) => {
 
 	return await api.post<HTTPResponse>(userRoutes.changePassword, body);
 };
+
+export const getAllUsers = async (params: string = '') =>
+	await api.get(`${userRoutes.users}?${params}`);
+
+export const createUser = async (body: UserFormValue) =>
+	await api.post(userRoutes.users, body);
+
+export const updateUser = async (id: number, body: UserFormValue) =>
+	await api.put(buildURL(userRoutes.userId, { id }), body);
+
+export const showUser = async (id: number) =>
+	await api.get(buildURL(userRoutes.userId, { id }));
--- src/features/users/api/routes.ts
+++ src/features/users/api/routes.ts
@@ -1,5 +1,7 @@
 const userRoutes = {
 	changePassword: '/v1/users/change-password',
+	users: '/v1/users',
+	userId: '/v1/users/${id}',
 };
 
 export default userRoutes;
